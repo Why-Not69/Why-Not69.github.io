6b7f282821563d0790f93a979d68312f69dc1fcb
Why-Not69 <95434062+Why-Not69@users.noreply.github.com>
2024-09-09 15:11:00 +0200

    Update main_copy.py

--- main_copy.py
+++ main_copy.py
@@ -1,29 +1,26 @@
+import os
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Добавь это
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Разреши CORS
+CORS(app)
 
-# Хранилище сообщений
 messages = []
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    print("Received GET request for /messages")
-    print("Returning messages:", messages)
     return jsonify(messages)
-
 
 @app.route('/messages', methods=['POST'])
 def add_message():
     message = request.json.get('message')
-    sender = request.json.get('sender')
-    print("Received POST request with message:", message, "from sender:", sender)  # Логирование данных
+    sender = request.json.get('sender', 'unknown')
     if message:
         messages.append({"message": message, "sender": sender})
+        print(f"Received POST request with message: {message} from sender: {sender}")
         return jsonify({"status": "success", "message": "Message added"}), 201
     return jsonify({"status": "error", "message": "No message provided"}), 400
 
-
 if __name__ == '__main__':
-    app.run(port=8000)
+    port = int(os.environ.get('PORT', 8000))  # Use PORT environment variable or default to 8000
+    app.run(host='0.0.0.0', port=port)
